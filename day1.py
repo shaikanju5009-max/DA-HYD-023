809f7b083909c94b565a01e2e835eed53ebd3233
shaik asif <shaikanju5009@gmail.com>
2026-07-28 15:18:52 +0530

    Tokens - Day1

---
+++ day1.py
@@ -0,0 +1,16 @@
+#print("Hello codegnan")
+'''
+tokens
+keywords
+identifiers
+'''
+name='anjum'
+print(name)
+email_id='shaikanju5009@gmail.com'
+print(Email_id)
+you=anjum
+print('you')
+name,location,age='anjum','hyd',21
+print(name)
+print(location)
+print(age)
