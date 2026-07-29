ae1079e3983d8e3d6147d5a1a922aa1475fda2d7
shaik asif <shaikanju5009@gmail.com>
2026-07-29 13:05:34 +0530

    numericdatatypes-typecasting

---
+++ day2.py
@@ -0,0 +1,107 @@
+'''
+tokens-->vairable,punchuation
+vairables-->name memory location,its a placeholder for data
+
+#multiassing of vairables
+name,age,place='codegnan',7,'hyderabad'
+print(name,age,place,sep=',')
+#valueerror to many values to unpack
+#reassing vairables
+name="codegnan"
+a,b=45,1.5
+print(a,b)
+a,b=b,a#swapping
+print(a,b,sep=',')
+#deleting the vairable -->del
+#del a
+#print(a)
+#punchators -->{}(dist,sets) ,[](lists),()(tuples)
+name = "codegnan"; age = 7
+print(name,age)
+#datatype--> numeric(int,float,complex)boolean,None
+#          --> sequences--.list,tuples,sets,strings,
+                           #frozensets,mapping(dict)
+age=7
+print(type(age))#vairable
+print(type(234))#letriol
+#quantity = 03 #it is not allowed to start with zero
+#float-->temp,salary,price,tax
+price =120.23;discount=2.3
+print(type(price))
+#complex--> combination of real and image
+data=3+i4
+i4=4
+
+print(data)
+
+data=5+2j #j is imag represntation
+print(type(data))
+#boolean-->True/Flase
+valid = True
+print(type(valid))
+#typecasting--> converting one type to another type
+#python by default follows implicit type(we need not to mention the datatype)
+#every builtin datatype is a built in functtion
+age = 35
+print(type(age))
+b= float(age)
+print(b)
+c= complex(age)
+print(c)
+d= bool(age)
+print(d)
+ #float-->typecasting
+age = 35.5
+print(type(age))
+b= int(age)
+print(b)
+c= complex(age)
+print(c)
+d= bool(age)
+print(d)
+#bool of anything is true ,,nothing is false
+#comlex-->typecaste
+
+data = 2+5j
+print(type(data))
+#b= int(data)#typeError
+#print(b)
+#c= complex(data)
+#print(c)
+
+d= bool(data)
+print(d)
+'''
+e = int(float(bool(45)))
+print(e)
+e =25 + 3.5+ 4j+ False
+print(e)
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
